8a6d37df6709905c6ddea512eec3a5e75d79fe26
brightlightkim <taeyangk95@naver.com>
2022-02-23 08:17:26 -0700

    finished writing code for the basic structure

--- script.js
+++ script.js
@@ -1,17 +1,145 @@
-createHeader();
-
-var currentTime = {
-    days: 0,
-    hours: 0, 
-    minutes: 0,
-    seconds: 0,
+/**Setting Head of HTML*/
+function setHeadHTML(){
+    const responsiveSetting = document.createElement('meta');
+    responsiveSetting.name = "viewport";
+    responsiveSetting.content = "width=device-width, initial-scale=1.0";
     
-    currentTime: function() {
-        var today = new Date();
-        this.days = today.getDay();
-        this.hours = today.getHours();
-        this.minutes = today.getMinutes();
-        this.seconds = today.getSeconds();
+    const styleLink = document.createElement('link');
+    styleLink.rel = "stylesheet";
+    styleLink.href = "./styles.css";
+
+    const title = document.createElement('title');
+    title.innerHTML = "Vivint";
+
+    document.head.appendChild(responsiveSetting);
+    document.head.appendChild(styleLink);
+    document.head.appendChild(title);
+}
+
+var countDownTime = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 20,
+    done: false
+}
+
+/**Time Function */
+
+/**Setting Count Time with current Time (11PM - currentTime) 
+ * This can be changed with any given time.
+*/
+
+
+function timeCalculator(firstTime, subtractTime){
+    if (firstTime[1] < subtractTime[1]){
+        firstTime[0] = 1;
+        /**This is for the case when it's 23:50. 
+         * In this case, we put 
+         * 00 23 00 00 
+         * 00 23 50 40
+         * 
+         * 01 23 00 00
+         * 00 23 50 40
+         * 
+         * 01 00 10 20
+         */
+    }
+}
+
+function settingCountDownTimeWith11PM_MDT() {
+    var currentTime = new Date();
+    var firstTime = [0, 23, 0, 0]; /**This can be changed with any given time */
+    var subtractTime = [0, currentTime.getHours(), currentTime.getMinutes(), currentTime.getSeconds()];
+    timeCalculator(firstTime, subtractTime);
+}
+
+/**Day, Minute, Hour, Seconds */
+function getClassData(className, index) {
+    return document.getElementsByClassName(className)[index].firstChild.innerHTML;
+}
+
+/**60 */
+function changeSecond() {
+    var second = parseInt(getClassData("time-box", 3));
+    
+    if (second === 0) {
+        if (!changeMinute()) {
+            return false;
+        }
+        second = 59;
+    }
+    else{
+        second--;
+    }
+    
+    document.getElementsByClassName("time-box")[3].firstChild.innerHTML = second;
+    return true;
+}
+
+/**60 */
+function changeMinute() {
+    var minute = parseInt(getClassData("time-box", 2));
+    
+    if (minute === 0) {
+        if (!changeHour()) {
+            return false;
+        }
+        minute = 59;
+    }
+    else{
+        minute--;
+    }
+    document.getElementsByClassName("time-box")[2].firstChild.innerHTML = minute;
+    return true;
+}
+
+/**24 */
+function changeHour() {
+    var hour = parseInt(getClassData("time-box", 1));
+   
+    if (hour === 0) {
+        if (!changeDay()) {
+            return false;
+        }
+        hour = 23;
+    }
+    else{
+        hour--;
+    }
+    document.getElementsByClassName("time-box")[1].firstChild.innerHTML = hour;
+    return true;
+}
+
+/**whatever */
+function changeDay() {
+    var day = parseInt(getClassData("time-box", 0));
+    
+    if (day === 0) {
+        return false;
+    }
+    else{
+        day--;
+    }
+    document.getElementsByClassName("time-box")[0].firstChild.innerHTML = day;
+    return true;
+}
+
+function remove(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+/**Time change */
+function countDown() {
+    countDownTime.done = changeSecond();
+
+    if (!countDownTime.done) {
+        remove(document.getElementsByClassName("banner")[0]);
+        var element = document.getElementsByClassName("banner")[0];
+        element.parentNode.removeChild(element);
+        clearInterval(countInterval);
     }
 }
 
@@ -38,13 +166,13 @@ function createImageLink(url, imgPath, className) {
     return link;
 }
 
-function createSpan(message){
+function createSpan(message) {
     const span = document.createElement('span');
     span.innerHTML = message;
     return span;
 }
 
-function createP(message){
+function createP(message) {
     const p = document.createElement('p');
     p.innerHTML = message;
     return p;
@@ -52,7 +180,7 @@ function createP(message){
 
 /**Main Message Helping Functions */
 
-function createClickButton(){
+function createClickButton() {
     const button = document.createElement('a');
     const img = document.createElement('img');
     const span = createSpan("877.537.3785");
@@ -65,7 +193,7 @@ function createClickButton(){
     return button;
 }
 
-function createExtraText(){
+function createExtraText() {
     const extra_text = createDiv("extra-text");
     extra_text.innerHTML = "*With qualifiying system purchase.&nbsp;";
 
@@ -83,18 +211,18 @@ function createTimeBox(time_type) {
     const span_message = createSpan(time_type);
 
     const p = createP('');
-    switch(time_type){
+    switch (time_type) {
         case "days":
-            p.innerHTML = currentTime.days;
+            p.innerHTML = countDownTime.days;
             break;
         case "hours":
-            p.innerHTML = currentTime.hours;
+            p.innerHTML = countDownTime.hours;
             break;
         case "minutes":
-            p.innerHTML = currentTime.minutes;
+            p.innerHTML = countDownTime.minutes;
             break;
         default:
-            p.innerHTML = currentTime.seconds;
+            p.innerHTML = countDownTime.seconds;
             break;
     }
 
@@ -105,8 +233,7 @@ function createTimeBox(time_type) {
 
 function createTimer() {
     const timer = createDiv("timer");
-    
-    currentTime.currentTime();
+
     const days = createTimeBox("days");
     const hours = createTimeBox("hours");
     const minutes = createTimeBox("minutes");
@@ -116,7 +243,7 @@ function createTimer() {
     timer.appendChild(hours);
     timer.appendChild(minutes);
     timer.appendChild(seconds);
-    
+
     return timer;
 }
 
@@ -132,7 +259,7 @@ function createHeader() {
 }
 
 /*ContentWrapper Functions*/
-function createMainMessage(){
+function createMainMessage() {
     const content_wrapper = createDiv("content-wrapper");
     const content = createDiv("content");
     const small_text_1 = createDivWithText("small-text", "HOME SECURITY SALE");
@@ -156,7 +283,7 @@ function createMainMessage(){
 
 /* Banner*/
 
-function createTimeBanner(){
+function createTimeBanner() {
     const banner = createDiv("banner");
     const text_box = createDivWithText("text-box", "Get Offer Before Close");
     const timer = createTimer();
@@ -169,6 +296,12 @@ function createTimeBanner(){
 
 /* Every Functions for Landing Webpage*/
 
+setHeadHTML();
 createHeader();
 createMainMessage();
 createTimeBanner();
+var countInterval = setInterval(function(){
+    countDown();
+}, 1000);
+
+
